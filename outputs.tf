0fd2d717565fc0f51341ce87efa00ba010388dca
JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>
2026-05-19 11:08:44 -0600

    Añadido status a los outputs
    Signed-off-by: JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "lxc_info" {
   description = "Información de los LXC creados"
-  value       = { for name in var.lxc_names : name => { id = proxmox_virtual_environment_container.lxc[name].vm_id } }
+  value       = { for name in var.lxc_names : name => {
+    id     = proxmox_virtual_environment_container.lxc[name].vm_id
+    status = proxmox_virtual_environment_container.lxc[name].status
+  } }
 }
 
 output "lxc_ids" {
