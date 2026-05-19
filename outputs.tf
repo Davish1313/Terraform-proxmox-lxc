10df9cb12db86d134ee2e1bb20e1942676480a5a
JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>
2026-05-19 11:16:43 -0600

    change atribute status to node-name
    Signed-off-by: JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,8 @@ output "lxc_info" {
   description = "Información de los LXC creados"
   value       = { for name in var.lxc_names : name => {
     id     = proxmox_virtual_environment_container.lxc[name].vm_id
-    status = proxmox_virtual_environment_container.lxc[name].status
+    name   = name
+    node   = proxmox_virtual_environment_container.lxc[name].node_name
   } }
 }
 
