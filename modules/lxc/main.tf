b6d3bf3bd8fdb07433fce81d1ebc875e01e6eda7
JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>
2026-05-23 19:46:59 -0600

    version = ">= 0.71.0" (requiere hook_script_file_id)
    Signed-off-by: JAIR DAVISH <jairdavish@MacBook-Air-de-JAIR.local>

--- modules/lxc/main.tf
+++ modules/lxc/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = ">= 0.66.0"
+      version = ">= 0.71.0"
     }
   }
 }
@@ -11,15 +11,38 @@ locals {
   disk_value = tonumber(substr(var.disk_size, 0, length(var.disk_size) - 1))
   disk_unit  = substr(var.disk_size, length(var.disk_size) - 1, 1)
   disk_gb    = local.disk_unit == "M" ? ceil(local.disk_value / 1024) : local.disk_value
+  pkg_install = length(var.ansible_packages) > 0 ? join(" ", var.ansible_packages) : ""
+}
+
+resource "proxmox_virtual_environment_file" "ansible_hook_script" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.proxmox_node
+
+  source_raw {
+    data = <<-SCRIPT
+#!/bin/sh
+if [ "$1" = "post-start" ]; then
+  pct exec $3 -- bash -c "apt-get update -qq && apt-get install -y -qq python3 ${local.pkg_install}"
+  pct exec $3 -- bash -c "sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config"
+  pct exec $3 -- systemctl restart sshd
+fi
+SCRIPT
+    file_name = "ansible-hook-script.sh"
+  }
+
+  file_mode = "0755"
 }
 
 resource "proxmox_virtual_environment_container" "lxc" {
-  for_each     = toset(var.lxc_names)
-  description  = "LXC Container - ${each.value}"
-  node_name    = var.proxmox_node
-  unprivileged = true
-  started      = true
-  tags         = ["terraform", var.lxc_project]
+  for_each = toset(var.lxc_names)
+
+  description         = "LXC Container - ${each.value} (Ansible-ready)"
+  node_name           = var.proxmox_node
+  unprivileged        = true
+  started             = true
+  tags                = ["terraform", var.lxc_project, "ansible-ready"]
+  hook_script_file_id = proxmox_virtual_environment_file.ansible_hook_script.id
 
   initialization {
     hostname = each.value
@@ -40,6 +63,10 @@ resource "proxmox_virtual_environment_container" "lxc" {
   }
 
   protection = true
+
+  features {
+    nesting = true
+  }
 
   cpu {
     cores = var.vcpu
